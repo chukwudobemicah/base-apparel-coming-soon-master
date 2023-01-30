6dfaff31e0be35ce89e31290a95c293841095d76
chukwudobemicah <82509708+chukwudobemicah@users.noreply.github.com>
2023-01-30 08:35:08 +0100

    Add files via upload

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,22 +1,23 @@
 const errorMessage = document.querySelector('.error-message');
 const input = document.querySelector('input');
 const form = document.querySelector('form');
-const regexEmail =
-  /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/;
 
-const clearErrorMessage = function () {
-  errorMessage.textContent = '';
+const validateEmail = function (email) {
+  const regex = /^[a-zA-Z][a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z.]{2,}$/;
+  return regex.test(email);
 };
 
+const clearErrorMessage = function () {
+  return (errorMessage.textContent = '');
+};
+const addErrorMessage = function () {
+  errorMessage.textContent = 'Email accepted';
+  errorMessage.style.color = 'green';
+};
 const checkEmail = function (inputValue) {
-  if (inputValue === '') {
+  if (inputValue === '' && !validateEmail(inputValue)) {
     errorMessage.classList.remove('hidden');
     input.classList.add('input-error');
-  } else {
-    errorMessage.classList.remove('hidden');
-    clearErrorMessage();
-    errorMessage.textContent = 'Email accepted';
-    errorMessage.style.color = 'green';
   }
 };
 
@@ -24,11 +25,3 @@ form.addEventListener('submit', function (e) {
   e.preventDefault();
   checkEmail(input.value);
 });
-// const micah = 'dobemike101@gmail.com';
-// console.log(micah.match(regexEmail));
-// console.log(!micah.match(regexEmail));
-// console.log(micah);
-
-// clearErrorMessage();
-// errorMessage.textContent = 'Email accepted';
-// console.log(errorMessage);
